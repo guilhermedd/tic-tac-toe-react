9a94e95914c24a6f624d34fe3bea8c0fd83afb95
Guilherme Diel <guiddiel@gmail.com>
2024-02-08 08:15:34 -0300

    layout

--- src/Board.js
+++ src/Board.js
@@ -1,18 +1,35 @@
 import './App.css';
 import Square from "./components/Square";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function Board() {
-  const indexes = [1, 2, 3];
   return (
-    <>
-      {
-        indexes.map((i)=>
-            (
-              indexes.map((index)=>
-                (<Square index={i * index}/>))
-            )
-        )
-      }
-    </>
+      <>
+          <Container fluid className="board">
+              <Row className="justify-content-md-center">
+                  <Col md="auto">
+                      <Square index={1}/>
+                      <Square index={2}/>
+                      <Square index={3}/>
+                  </Col>
+              </Row>
+              <Row className="justify-content-center">
+                  <Col md="auto">
+                      <Square index={1}/>
+                      <Square index={2}/>
+                      <Square index={3}/>
+                  </Col>
+              </Row>
+              <Row className="justify-content-center">
+                  <Col md="auto">
+                      <Square index={1}/>
+                      <Square index={2}/>
+                      <Square index={3}/>
+                  </Col>
+              </Row>
+          </Container>
+      </>
   );
 }
 
